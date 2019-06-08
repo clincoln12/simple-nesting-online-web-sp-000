93d8037e3b3ee177dd8b595eb15d09b10f549287
Chris Lincoln <chris.lincoln12@gmail.com>
2019-06-08 16:10:32 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,8 @@ def dennis_ritchies_language
         }
      }
      
-  programmer_hash[:dennis_ritchie][:languages]
+  programmer_hash[:dennis_ritchie][:languages].each do |k, v|
+    puts "#{v}"
 end
 
 def adding_matz
